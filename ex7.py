4c16ba7a3d890b07c2d628865578da525be47f25
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 10:27:41 +0100

    ex7 création du fichier

---
+++ ex7.py
@@ -0,0 +1,2 @@
+# EXERCICE 7
+# Écrire une fonction qui calcule le montant total dépensé par tous les clients.
---
+++ ex9.py
@@ -0,0 +1,2 @@
+# EXERCICE 9
+# Écrivez un script qui analyse les titres des e-mails marketing pour trouver le nombre d'occurrences d'un mot clé spécifique.
