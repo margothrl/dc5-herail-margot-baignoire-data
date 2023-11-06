2ba35ea43fdd5b3319453c19938aafdbb3ac8584
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 10:38:40 +0100

    ex10 création du fichier

---
+++ ex10.py
@@ -0,0 +1,2 @@
+# EXERCICE 10
+# Calculez la moyenne, la médiane et l'écart-type des clics obtenus à partir d'une liste de campagnes publicitaires (Vous avez la liberté de définir les kpi nécessaires).
