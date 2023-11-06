df59862c3ff7e42978e7038bd13b184fadfc4a53
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 09:27:45 +0100

    ex5 fait

---
+++ ex5.py
@@ -0,0 +1,17 @@
+# EXERCICE 5
+# Créer une liste de dictionnaires, où chaque dictionnaire contient des informations sur un client (nom, email, montant dépensé). Minimum 50 entrées
+
+liste_clients = []
+
+for i in range(1, 51):
+    client = {
+        "nom": f"Client {i}",
+        "email": f"client{i}@exemple.com",
+        "montant_depense": 1000 + i * 100 
+    }
+    liste_clients.append(client)
+
+for client in liste_clients:
+    print(f"Nom : {client['nom']}, Email : {client['email']}, Montant dépensé : {client['montant_depense']} dollars")
+
+input("Appuyez sur Entrée pour quitter.")
