e19128ff1986097810df7dadc91dc5b07699b22d
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 08:25:42 +0100

    ex1-a fait

---
+++ ex1-a.py
@@ -0,0 +1,7 @@
+nom_entreprise = "SNCF Voyageurs"
+nombre_clients = 100
+revenu_moyen_par_client = 50
+
+revenu_total = nombre_clients * revenu_moyen_par_client
+
+print(f"Le revenu total de {nom_entreprise} est de {revenu_total} euros.")
