0c186bb3e0454dd96e93d69242df0617dc236c1c
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 08:44:42 +0100

    ex2-a modifié

--- ex2-a.py
+++ ex2-a.py
@@ -1,6 +1,10 @@
 # EXERCICE 2 - a
 # Créez une liste des dépenses marketing mensuelles et calculez les dépenses totales de l'année.
 
-depenses_mensuelles_marketing = [50, 1200, 800, 1500, 1300, 1100, 900, 100, 950, 1200, 1400, 1100]
-depenses_annuelles_marketing = sum(depenses_mensuelles_marketing)
-print(f"Les dépenses marketing totales de l'entreprise sur toute l'année sont de {depenses_annuelles_marketing}€.")
+depenses_mensuelles = [500, 1200, 800, 1500, 1300, 1100, 90, 1000, 950, 1200, 1400, 1100]
+
+depenses_annuelles = 0
+for depense in depenses_mensuelles:
+    depenses_annuelles += depense
+
+print(f"Les dépenses marketing totales de l'entreprise sur l'année sont de {depenses_annuelles}€.")
---
+++ ex2-b.py
@@ -0,0 +1,2 @@
+# EXERCICE 2 - b
+# Listes : Créez une liste de 10 nombres, trouvez le maximum, le minimum, et calculez la moyenne.
