d8285228e7be49e664a135809f1a6606a3cbc51a
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 08:53:10 +0100

    ex2-b fait

--- ex2-b.py
+++ ex2-b.py
@@ -1,2 +1,25 @@
 # EXERCICE 2 - b
 # Listes : Créez une liste de 10 nombres, trouvez le maximum, le minimum, et calculez la moyenne.
+
+liste_de_nombres = [15, 22, 17, 30, 62, 48, 20, 35, 51, 89]
+
+
+maximum = liste_de_nombres[0]  
+for nombre in liste_de_nombres:
+    if nombre > maximum:
+        maximum = nombre
+
+minimum = liste_de_nombres[0] 
+for nombre in liste_de_nombres:
+    if nombre < minimum:
+        minimum = nombre
+
+somme = 0
+for nombre in liste_de_nombres:
+    somme += nombre
+moyenne = somme / len(liste_de_nombres)
+
+
+print(f"Le maximum est : {maximum}")
+print(f"Le minimum est : {minimum}")
+print(f"La moyenne est : {moyenne}")
