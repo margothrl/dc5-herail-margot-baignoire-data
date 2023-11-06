5aeb34033ac2014d9c737006510732132c3a1a9d
Margot Herail <margot.herail@hotmail.fr>
2023-11-06 09:31:10 +0100

    ex5 modification

--- ex5.py
+++ ex5.py
@@ -7,11 +7,11 @@ for i in range(1, 51):
     client = {
         "nom": f"Client {i}",
         "email": f"client{i}@exemple.com",
-        "montant_depense": 1000 + i * 100 
+        "montant_depense": 10 + i * 5
     }
     liste_clients.append(client)
 
 for client in liste_clients:
-    print(f"Nom : {client['nom']}, Email : {client['email']}, Montant dépensé : {client['montant_depense']} dollars")
+    print(f"Nom : {client['nom']}, Email : {client['email']}, Montant dépensé : {client['montant_depense']}€")
 
 input("Appuyez sur Entrée pour quitter.")
---
+++ ex6.py
@@ -0,0 +1,2 @@
+# EXERCICE 6
+# Parcourir la liste des clients et afficher ceux qui ont dépensé plus de 100€.
